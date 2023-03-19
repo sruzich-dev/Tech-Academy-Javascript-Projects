a2036bbfd02b11a38e5fa52b355ff27df088b56a
sruzich-dev <sabrina.ruzich@gmail.com>
2023-03-19 13:27:18 -0700

    Adding files to TechAcademy Javascript Project Repo

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,43 @@
+/*~~~~VARIABLES, STATEMENTS & EXPRESSIONS in JavaScript ~~~~*/
+
+
+/* Creating 2 VARIABLES with STRING values which are concatenated */
+var Sent1 = "This is the beginning of the string";
+var Sent2 = "and this is the end of the string";
+    document.write()
+
+/*-- DECLARING VARIABLE with STRING value*/
+var A = "This is a string"; 
+
+
+/*-- This is a windoW.alert STATEMENT 
+with a STRING EXPRESSION.
+This STATEMENT type tells the computer to display whatever is input as parameters (inside parenthesis) 
+as a pop-up window on the users screen--*/
+window.alert("Hello, World!");
+
+
+/*-- This is a "document.write" STATEMENT, 
+contains a STRING EXPRESSION utilizing special characters ("") represented by a "\".
+This type of STATEMENT displays whatever PARAMETERS (inside parenthesis) on the users screen--*/
+document.write("She asked the man, \"What time does the movie start?\"<br>\"Nine thirty!\" He replied.");
+
+/*-- This is document.write STATEMENT,
+contains a STRING CONCATENATED STATEMENT 
+utilizes special characters. 
+Linking mutliple STRING EXPRESSIONS together using the "+" symbol --*/
+document.write("\"To Be"
++ " or not to be,"
++ " that is the question.\""
++ "-Shakespeare");
+ 
+/*Declaring a VARIABLE with CONCATENATED STRING expression using document.write STATEMENT to display completed STRING EXPRESSION.*/
+var B = "Hello" + "String"
+document.write(B)
+
+/*-- Assigning multiple VARIABLES with document.write statement to display the value of "Class" variable --*/
+var School = "Highschool", Class = "Gym", extraCredit = "Biology";
+document.write(Class)
+
+/* This is a way to write and display (on-screen) an EXPRESSION in JavaScript */
+document.write(1 + 3);
